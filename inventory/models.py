56710b725107dd5fa5760671c5a176ea9eed6c27
Fausto Oliveri <ofausto01@gmail.com>
2023-02-22 20:04:13 -0300

    Models, Migration and SuperUser

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+# Menu item, items and price set for each entry
+class MenuItem(models.model):
+    title= models.CharField(max_length=100, unique=True)
+    price= models.FloatField(default=0)
+# ingredient model, ing, available quantity, price per unit
+class Ingredient(models.model):
+    name = models.CharField(max_length=100, unique=True)
+    quantity= models.FloatField(default=0)
+    unit= models.CharField(max_length=100)
+    price_per_unit= models.FloatField(default=0)
+
+# recipe requirement, list of ingr that each menu req
+class RecipeRequirement(models.model):
+    menu_item = models.ForeignKey(MenuItem, on_delete= models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete= models.CASCADE)
+    quantity = models.FloatField(default=0)
+    
+# purchase, log of all purchases made
+class Purchase(models.model):
+    menu_item = models.ForeignKey(MenuItem, on_delete= models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
