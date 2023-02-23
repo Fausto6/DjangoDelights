265bb6ed4733f306f702bf936d823066861b2a76
Fausto Oliveri <ofausto01@gmail.com>
2023-02-22 23:01:33 -0300

    Finally adding the models and migrating

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import RecipeRequirement, MenuItem, Ingredient, Purchase
+from .models import MenuItem, Ingredient, RecipeRequirement, Purchase
 
-[admin.site.register(X) for X in [RecipeRequirement, MenuItem, Ingredient, Purchase]]
+class MenuItemAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(MenuItem)
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Ingredient)
+
+class RecipeRequirementAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(RecipeRequirement)
+
+class PurchaseAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Purchase)    
--- inventory/models.py
+++ inventory/models.py
@@ -3,23 +3,23 @@ from django.db import models
 # Create your models here.
 
 # Menu item, items and price set for each entry
-class MenuItem(models.model):
+class MenuItem(models.Model):
     title= models.CharField(max_length=100, unique=True)
     price= models.FloatField(default=0)
 # ingredient model, ing, available quantity, price per unit
-class Ingredient(models.model):
+class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
     quantity= models.FloatField(default=0)
     unit= models.CharField(max_length=100)
     price_per_unit= models.FloatField(default=0)
 
 # recipe requirement, list of ingr that each menu req
-class RecipeRequirement(models.model):
+class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete= models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete= models.CASCADE)
     quantity = models.FloatField(default=0)
     
 # purchase, log of all purchases made
-class Purchase(models.model):
+class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete= models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
