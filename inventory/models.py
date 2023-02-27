efbea7a56a7a0cc16d4489dc1c5dea172b150358
Fausto Oliveri <ofausto01@gmail.com>
2023-02-27 18:54:27 -0300

    Added custom methods to the models

--- inventory/models.py
+++ inventory/models.py
@@ -6,6 +6,16 @@ from django.db import models
 class MenuItem(models.Model):
     title= models.CharField(max_length=100, unique=True)
     price= models.FloatField(default=0)
+
+    def get_absolute_url(self):
+        return "/menu"
+    
+    def available(self):
+        return all(X.enough() for X in self.reciperequirement_set.all())
+
+    def __str__(self):
+        return f"title={self.title}; price={self.price}"
+    
 # ingredient model, ing, available quantity, price per unit
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -13,13 +23,40 @@ class Ingredient(models.Model):
     unit= models.CharField(max_length=100)
     price_per_unit= models.FloatField(default=0)
 
+    def get_absolute_url(self):
+        return "/ingredients"
+    
+
+    def __str__(self):
+        return f"""
+        name={self.name};
+        qty={self.quantity};
+        unit={self.unit};
+        unit_price={self.price_per_unit}
+        """
+    
 # recipe requirement, list of ingr that each menu req
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete= models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete= models.CASCADE)
     quantity = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"menu_item=[{self.menu_item.__str__()}]; ingredient={self.ingredient.name}; qty={self.quantity}"
+    
+    def get_absolute_url(self):
+        return "/menu"
+
+    def enough(self):
+        return self.quantity <= self.ingredient.quantity
     
 # purchase, log of all purchases made
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete= models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"menu_item=[{self.menu_item.__str__()}]; time={self.timestamp}"
+
+    def get_absolute_url(self):
+        return "/purchases"
